4a8279f9ef346f3b52228d5c5e3f62e440c5fb79
Augustohfa <azevedogestoes@gmail.com>
2022-05-14 15:36:45 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Scanner sc = new Scanner(System.in);
+			
+//		int minutos = sc.nextInt();
+//		
+//		double conta = 50.00;
+//		if (minutos > 100) {
+//			conta += (minutos - 100) *  2.0;
+//		}
+//			System.out.println("Valor da conta = R$" + conta);
+		//Declaração de Variáveis
+		int x = sc.nextInt();
+		
+		String dia;
+		
+		
+		
+	
+		
+			switch(x) {
+			case 1:
+				dia = "Domingo";
+				break;
+			case 2:
+				dia = "Segunda";
+				break;
+			case 3:
+				dia = "Terça";
+				break;
+			case 4:
+				dia = "Quarta";
+				break;
+			case 5:
+				dia = "Quinta";
+				break;
+			case 6:
+				dia = "Sexta";
+				break;
+			case 7:
+				dia = "Sábado";
+				break;
+			default:
+				dia = "Valor inválido";
+			}
+			sc.close();
+			System.out.println("Dia da semana: " + dia);
+	
+	
+	
+	
+	
+	}
+
+}
