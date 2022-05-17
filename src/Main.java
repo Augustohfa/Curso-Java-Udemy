2ac59bc8feeee867d6b0a3ee62a562e9bafca1f4
Augustohfa <azevedogestoes@gmail.com>
2022-05-17 07:43:46 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -62,22 +62,42 @@ public class Main {
 		
 		
 		// Ex: Programa só para quando quando o usuário digitar o número 0 e depois soma todos itens digitados.
+//		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int x = sc.nextInt();
+//		int soma = 0;
+//		while(x != 0) {// Enquanto o que estiver dentro do () for True ele retorna aos comandos dentro da chave
+//			soma += x;
+//			x = sc.nextInt(); 
+//		}
+//		
+//		System.out.println(soma);
+//		sc.close();
+				
 		
-		/*Scanner sc = new Scanner(System.in);
-		
-		int x = sc.nextInt();
-		int soma = 0;
-		while(x != 0) {// Enquanto o que estiver dentro do () for True ele retorna aos comandos dentro da chave
-			soma += x;
-			x = sc.nextInt();
-		}
-		
-		System.out.println(soma);
-		sc.close();
-		*/
+			//Estrutura Repetitiva FOR
+			//Ex: Mesmo do while, porém com número de somas determinada!
+Scanner sc = new Scanner(System.in);
+		//for(inicio(executa uma vez);condicao V: executa e volta F:Sai do loop ;incremente: executa sempre que voltar no loop) {
+			//comando 1
+			//comando 2
+		//}
+//int N = sc.nextInt();
+//int soma = 0;
+//		
+//	for(int i = 0; i < N; i++) {
+//		int x = sc.nextInt();
+//		soma += x;
+//	}
+//System.out.println(soma);
+	
+	for(int i = 0; i<=10; i++) {
+		System.out.println(i);
+	}
+sc.close();
 		
 	}
-
 }
 
 		
