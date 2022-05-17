eab4f2ee37dec444c8ecb0f64faed142867ef059
Augustohfa <azevedogestoes@gmail.com>
2022-05-17 06:14:17 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		
 		
 		
-		Scanner sc = new Scanner(System.in);
+		// Scanner sc = new Scanner(System.in);
 			
 //		int minutos = sc.nextInt();
 //		
@@ -15,46 +15,73 @@ public class Main {
 //		}
 //			System.out.println("Valor da conta = R$" + conta);
 		//Declaração de Variáveis
+//		int x = sc.nextInt();
+//		
+//		String dia;
+//		
+//		
+//		
+//	
+//		
+//			switch(x) {
+//			case 1:
+//				dia = "Domingo";
+//				break;
+//			case 2:
+//				dia = "Segunda";
+//				break;
+//			case 3:
+//				dia = "Terça";
+//				break;
+//			case 4:
+//				dia = "Quarta";
+//				break;
+//			case 5:
+//				dia = "Quinta";
+//				break;
+//			case 6:
+//				dia = "Sexta";
+//				break;
+//			case 7:
+//				dia = "Sábado";
+//				break;
+//			default:
+//				dia = "Valor inválido";
+//			}
+//			sc.close();
+//			System.out.println("Dia da semana: " + dia);
+//	
+//	
+//	
+//	
+/*	while(condicao){
+		comando 1
+		comando 2
+		}
+		*/
+		
+		
+		// Ex: Programa só para quando quando o usuário digitar o número 0 e depois soma todos itens digitados.
+		
+		/*Scanner sc = new Scanner(System.in);
+		
 		int x = sc.nextInt();
+		int soma = 0;
+		while(x != 0) {// Enquanto o que estiver dentro do () for True ele retorna aos comandos dentro da chave
+			soma += x;
+			x = sc.nextInt();
+		}
 		
-		String dia;
+		System.out.println(soma);
+		sc.close();
+		*/
 		
-		
-		
-	
-		
-			switch(x) {
-			case 1:
-				dia = "Domingo";
-				break;
-			case 2:
-				dia = "Segunda";
-				break;
-			case 3:
-				dia = "Terça";
-				break;
-			case 4:
-				dia = "Quarta";
-				break;
-			case 5:
-				dia = "Quinta";
-				break;
-			case 6:
-				dia = "Sexta";
-				break;
-			case 7:
-				dia = "Sábado";
-				break;
-			default:
-				dia = "Valor inválido";
-			}
-			sc.close();
-			System.out.println("Dia da semana: " + dia);
-	
-	
-	
-	
-	
 	}
 
 }
+
+		
+		
+		
+		
+		
