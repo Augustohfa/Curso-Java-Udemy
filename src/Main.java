06eb8200735f6fb8a680156369458e7e4d82e9b2
Augustohfa <azevedogestoes@gmail.com>
2022-06-03 07:52:21 -0300

    Iniciando as funções no Java

--- src/Main.java
+++ src/Main.java
@@ -78,29 +78,111 @@ public class Main {
 		
 			//Estrutura Repetitiva FOR
 			//Ex: Mesmo do while, porém com número de somas determinada!
-Scanner sc = new Scanner(System.in);
-		//for(inicio(executa uma vez);condicao V: executa e volta F:Sai do loop ;incremente: executa sempre que voltar no loop) {
-			//comando 1
-			//comando 2
-		//}
-//int N = sc.nextInt();
-//int soma = 0;
-//		
-//	for(int i = 0; i < N; i++) {
-//		int x = sc.nextInt();
-//		soma += x;
+//Scanner sc = new Scanner(System.in);
+//		//for(inicio(executa uma vez);condicao V: executa e volta F:Sai do loop ;incremente: executa sempre que voltar no loop) {
+//			//comando 1
+//			//comando 2
+//		//}
+////int N = sc.nextInt();
+////int soma = 0;
+////		
+////	for(int i = 0; i < N; i++) {
+////		int x = sc.nextInt();
+////		soma += x;
+////	}
+////System.out.println(soma);
+//	
+//	for(int i = 0; i<=10; i++) {
+//		System.out.println(i);
 //	}
-//System.out.println(soma);
-	
-	for(int i = 0; i<=10; i++) {
-		System.out.println(i);
-	}
-sc.close();
+//sc.close();
+//	
+		//Estrutura repetitiva "do while" 
 		
-	}
+//		Scanner sc = new Scanner(System.in);
+//		char Resposta;	
+//	do {
+//			System.out.print("Digite a temperatura em Celsius: ");
+//			double C = sc.nextDouble();
+//			double F = 9.0 * C / 5.0 + 32;
+//			System.out.println("Equivalente em Fahrenheit: " + F);
+//			System.out.println("Dejesa ver outra temperatura? (s/n)");
+//			Resposta = sc.next().charAt(0); 
+//	   }while(Resposta != 'n'); 
+//			
+//		sc.close();
+//	Scanner sc = new Scanner(System.in);
+//	
+//// Formatação Java
+//	
+//	String original = "abcde FGHIJ abc abc DEFG   ";
+//	
+//	String s01 = original.toLowerCase();
+//	
+//	String s02 = original.toUpperCase();
+//	
+//	String s03 = original.trim(); // tira espaços
+//	
+//	String s04 = original.substring(2);
+//	
+//	String s05 = original.replace('a', 'x');
+//	
+//	System.out.println(s01);
+//	System.out.println(original);
+//	System.out.println(s02);
+//	System.out.println(s03);
+//	System.out.println(s04);
+//	System.out.println(s05);
+//	
+//	//Operaçao Split
+//	
+//	String s = "Augusto Henrique Ferreira de Azevedo";
+//	String[] vect = s.split(" ");
+//	
+//	System.out.println("Nome: " + vect[0] + " " + vect[1]);
+//	System.out.println("Sobrenome: " + vect[2]+" "+ vect[3]+ " "+ vect[4]);
+//	
+//	sc.close();
+	
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Digite 3 números: ");
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		int c = sc.nextInt();
+//		
+//		int maior = max(a, b, c);
+//		
+//		mostrarResultado(maior);
+//		//como fazer funções no java
+//		
+//		sc.close();
+//		
+//		
+//	}
+//	public static int max(int n1, int n2, int n3) {
+//		int aux;
+//		if(n1>n2 && n1>n3) {
+//			aux = n1;
+//		}
+//		else if(n2>n3){
+//			aux = n2;
+//		}
+//		else {
+//			aux = n3;
+//		}
+//		return aux;
+//	}
+//
+//	public static void mostrarResultado(int value) {
+//		System.out.println("Maior número é o: "+value);
+	
+	
+	}	
+
+
 }
 
-		
+		  
 		
 		
 		
