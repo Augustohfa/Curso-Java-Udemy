0dafa020144dcd0deba139d6bc699ffd1a688d40
Augustohfa <azevedogestoes@gmail.com>
2022-06-09 08:01:41 -0300

    Adicionando Classes pra trabalhar com POO

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+
+import entities.Triangulo;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -175,6 +178,79 @@ public class Main {
 //
 //	public static void mostrarResultado(int value) {
 //		System.out.println("Maior número é o: "+value);
+
+		
+		
+		
+		
+//	double xA, xB, xC, yA, yB, yC;
+//	Scanner sc = new Scanner(System.in);
+//	System.out.println("Digite as medidas do triangulo X: ");
+//	
+//	xA = sc.nextDouble();
+//	xB = sc.nextDouble();
+//	xC = sc.nextDouble();
+//	System.out.println("Digite as medidas do triangulo Y: ");
+//	yA = sc.nextDouble();
+//	yB = sc.nextDouble();
+//	yC = sc.nextDouble();
+//
+//	double p = (xA + xB + xC) / 2.0;
+//	double areaX = Math.sqrt(p*(p-xA)*(p-xB)*(p-xC));
+//	
+//	p = (yA + yB + yC) / 2.0;
+//	double areaY = Math.sqrt(p*(p-yA)*(p-yB)*(p-yC));
+//	
+//	System.out.printf("Área do triangulo X: %4f%n", areaX);
+//	System.out.printf("Área do triangulo Y: %4f%n", areaY);
+//	
+//	if (areaX > areaY) {
+//		System.out.println("A maior area é: X");
+//	}
+//	else {
+//		System.out.println("A maior área é o: Y");
+//	}
+//sc.close();
+	
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite as medidas do triangulo X: ");
+		Triangulo x, y;
+		x = new Triangulo();
+		y = new Triangulo();
+		
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		System.out.println("Digite as medidas do triangulo Y: ");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+	
+		double p = (x.a + x.b + x.c) / 2.0;
+		double areaX = Math.sqrt(p*(p-x.a)*(p-x.b)*(p-x.c));
+		
+		p = (y.a + y.b + y.c) / 2.0;
+		double areaY = Math.sqrt(p*(p-y.a)*(p-y.b)*(p-y.c));
+		
+		System.out.printf("Área do triangulo X: %4f%n", areaX);
+		System.out.printf("Área do triangulo Y: %4f%n", areaY);
+		
+		if (areaX > areaY) {
+			System.out.println("A maior area é: X");
+		}
+		else {
+		System.out.println("A maior área é o: Y");
+		}
+	sc.close();
+		
+		
+		
+		
+	
+	
+	
+	
 	
 	
 	}	
